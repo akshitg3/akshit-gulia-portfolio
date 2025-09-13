a9fea7cfa610ea25004669c2a2f60f63b43f5d64
akshitg3 <akshit.gulia@outlook.com>
2025-09-13 16:54:20 -0400

    Added sample data

---
+++ src/data/data.ts
@@ -0,0 +1,88 @@
+export const links = [
+  {
+    name: "Home",
+    hash: "#home",
+  },
+  {
+    name: "About",
+    hash: "#about",
+  },
+  {
+    name: "Projects",
+    hash: "#projects",
+  },
+  {
+    name: "Skills",
+    hash: "#skills",
+  },
+  {
+    name: "Experience",
+    hash: "#experience",
+  },
+] as const;
+
+export const experiencesData = [
+  {
+    title: "sample text",
+    location: "sample text",
+    description: "sample text",
+    icon: null,
+    date: "sample text",
+  },
+  {
+    title: "sample text",
+    location: "sample text",
+    description: "sample text",
+    icon: null,
+    date: "sample text",
+  },
+  {
+    title: "sample text",
+    location: "sample text",
+    description: "sample text",
+    icon: null,
+    date: "sample text",
+  },
+] as const;
+
+export const projectsData = [
+  {
+    title: "sample text",
+    description: "sample text",
+    tags: ["sample text", "sample text", "sample text"],
+    imageUrl: null,
+  },
+  {
+    title: "sample text",
+    description: "sample text",
+    tags: ["sample text", "sample text", "sample text"],
+    imageUrl: null,
+  },
+  {
+    title: "sample text",
+    description: "sample text",
+    tags: ["sample text", "sample text", "sample text"],
+    imageUrl: null,
+  },
+] as const;
+
+export const skillsData = [
+  "HTML",
+  "CSS",
+  "JavaScript",
+  "TypeScript",
+  "React",
+  "Next.js",
+  "Node.js",
+  "Git",
+  "Tailwind",
+  "MongoDB",
+  "Redux",
+  "GraphQL",
+  "Apollo",
+  "Express",
+  "PostgreSQL",
+  "Python",
+  "Django",
+  "Framer Motion",
+] as const;
