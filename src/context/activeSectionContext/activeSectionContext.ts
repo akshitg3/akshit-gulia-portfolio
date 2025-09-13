e4993a9a780314f9385185d151e6c4363fa51694
akshitg3 <akshit.gulia@outlook.com>
2025-09-13 18:51:36 -0400

    Finished header component

---
+++ src/context/activeSectionContext/activeSectionContext.ts
@@ -0,0 +1,22 @@
+import type { SectionName } from "../../types/types";
+import { createContext, useContext } from "react";
+
+export type ActiveSectionContextType = {
+  activeSection: SectionName;
+  setActiveSection: React.Dispatch<React.SetStateAction<SectionName>>;
+  timeOfLastClick: number;
+  setTimeOfLastClick: React.Dispatch<React.SetStateAction<number>>;
+};
+
+export const ActiveSectionContext =
+  createContext<ActiveSectionContextType | null>(null);
+
+export function useActiveSectionContext() {
+  const context = useContext(ActiveSectionContext);
+  if (context === null) {
+    throw new Error(
+      "useActiveSectionContext must be used within an ActiveSectionContextProvider"
+    );
+  }
+  return context;
+}
--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,12 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App.tsx";
+import ActiveSectionContextProvider from "./context/activeSectionContext/activeSectionContextProvider";
 
-createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <ActiveSectionContextProvider>
+      <App />
+    </ActiveSectionContextProvider>
+  </StrictMode>
+);
