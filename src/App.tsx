cb132c2b66b3ea04640da3a4260d51d6ea87db3a
akshitg3 <akshit.gulia@outlook.com>
2025-09-16 05:07:42 -0400

    updated main files and dependencies

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,21 @@
-import Header from "./components/header";
+import Intro from "./components/intro";
+import Header from "./components/header.tsx";
+import Skills from "./components/skills.tsx";
+import Experience from "./components/experience.tsx";
+import Projects from "./components/projects.tsx";
+import Footer from "./components/footer.tsx";
+
 function App() {
   return (
-    <div className={`font-inter h-screen bg-gray-50 text-gray-950 relative`}>
-      <div className="bg-[#fbe2e3] absolute top-[-6rem] z-10 right-[11rem] h-[31.25rem] w-[31.25rem] rounded-full blur-[10rem] sm:w-[68.75rem]"></div>
-      <div className="bg-[#dbd7fb] absolute top-[-1rem] z-10 left-[-35rem] h-[31.25rem] w-[50rem] rounded-full blur-[10rem] sm:w-[68.75rem] md:left-[-33rem] lg:left-[-28rem] xl:left-[-15rem] 2xl:left-[-5rem]"></div>
+    <div className={`font-inter h-auto text-gray-950 relative pt-28 sm:pt-36`}>
       <Header />
+      <main className="flex flex-col items-center px-4">
+        <Intro />
+        <Skills />
+        <Experience />
+        <Projects />
+      </main>
+      <Footer />
     </div>
   );
 }
--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
-import ActiveSectionContextProvider from "./context/activeSectionContext/activeSectionContextProvider";
+import ActiveSectionContextProvider from "./context/activeSectionContext.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
